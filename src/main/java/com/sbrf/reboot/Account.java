3de3910572c5cf07a45159b8a2553d99786d5905
Maria <example@com>
2021-12-05 19:24:39 +0300

    Доработан класс Account.java

--- src/main/java/com/sbrf/reboot/Account.java
+++ src/main/java/com/sbrf/reboot/Account.java
@@ -2,8 +2,15 @@ package com.sbrf.reboot;
 
 public class Account {
     private String accountNm;
+    private Long clientId;
+
+    public Account(String newAccountNm, Long clientId) {
+        this.clientId=clientId;
+        this.accountNm=newAccountNm;
+    }
 
     public Account(String newAccountNm) {
+        this.clientId=1L;
         this.accountNm=newAccountNm;
     }
 
