fc53bffc0aa980a5a2b1b471d77adcb929f77a1a
Maria <example@com>
2021-12-07 18:46:03 +0300

    Удален класс IAccountRepository.java

--- src/main/java/com/sbrf/reboot/Account.java
+++ src/main/java/com/sbrf/reboot/Account.java
@@ -4,11 +4,6 @@ public class Account {
     private String accountNm;
     private Long clientId;
 
-    public Account(String newAccountNm, Long clientId) {
-        this.clientId=clientId;
-        this.accountNm=newAccountNm;
-    }
-
     public Account(String newAccountNm) {
         this.clientId=1L;
         this.accountNm=newAccountNm;
--- src/main/java/com/sbrf/reboot/IAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sbrf.reboot;
-
-import java.util.Set;
-
-public interface IAccountRepository {
-    Set<Account> repository = null;
-    Set<Account> getAllAccountsByClientId(long l);
-}
