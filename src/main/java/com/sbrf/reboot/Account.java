cadeded6612b8d0ccfa29f57cdf37e65f512f28f
Maria <example@com>
2022-01-12 17:24:16 +0300

    Создан класс AcccountUtils

--- src/main/java/com/sbrf/reboot/Account.java
+++ src/main/java/com/sbrf/reboot/Account.java
@@ -1,8 +1,14 @@
 package com.sbrf.reboot;
 
+import jdk.jshell.Snippet;
+
+import java.util.Date;
+
 public class Account {
     private String number;
     private long clientId;
+    private Date date;
+    private Integer balance;
 
     public Account(String number) {
         this.clientId=1L;
@@ -13,6 +19,10 @@ public class Account {
         this.clientId=clientId;
         this.number=number;
     }
+
+    public static Snippet builder() {
+    }
+
     public long getClientId() {
         return this.clientId;
     }
