3b7f6eea615dfdc6fe33e04b46880a49ceb5c566
Maria <example@com>
2022-02-15 22:54:15 +0300

    Удалено лишнее

--- src/main/java/com/sbrf/servlet/Servlet.java
+++ src/main/java/com/sbrf/servlet/Servlet.java
@@ -8,29 +8,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicInteger;
 
-//@WebServlet(name = "name")
-//public class Servlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        HttpSession session = req.getSession();
-//        Integer counter = (Integer) session.getAttribute("counter");
-//        if (counter == null) {
-//            counter = 0;
-//        }
-//        else {
-//            counter++;
-//        }
-//        session.setAttribute("counter", counter);
-//        String name = req.getParameter("name");
-//        resp.setContentType("text/html");
-//        PrintWriter printWriter = resp.getWriter();
-//        if (name != null) {
-//            printWriter.write("Привет, " + name + "<br>");
-//        }
-//        printWriter.write("Counter = " + counter);
-//        printWriter.close();
-//    }
-//}
 @WebServlet(name = "name")
 public class Servlet extends HttpServlet {
     @Override
