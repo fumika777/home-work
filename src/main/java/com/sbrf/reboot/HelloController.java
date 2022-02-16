19dc859db01714c3e27d3e04fe01cbdf4e7e8ef1
Maria <example@com>
2022-02-16 19:35:46 +0300

    Добавлено ДЗ 18

---
+++ src/main/java/com/sbrf/reboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.sbrf.reboot;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/user")
+    public String getMessage(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
